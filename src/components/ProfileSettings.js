b41dcd19acc610d79577b995593576f400ac1904
Steven Sanchez <steven12661@gmail.com>
2021-09-06 20:43:23 -0500

    Added Components Folder

---
+++ src/components/ProfileSettings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProfileSettings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Profile Settings</h1>
+            </div>
+        )
+    }
+}
