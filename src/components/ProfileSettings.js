f300071631b6a149d99b2262ffb0867343ad486d
Steven Sanchez <steven12661@gmail.com>
2021-09-08 22:35:44 -0500

    Adjusted profile info location

--- src/components/ProfileSettings.js
+++ src/components/ProfileSettings.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react'
-import JSONPretty from 'react-json-pretty'
+import React from 'react'
 
 
 function ProfileSettings(){
-    const user = sessionStorage.getItem('user')
     
     return (
         <div>
             <h1>Profile Settings</h1>
             
-            <JSONPretty data={user} />
 
 
         </div>
