6ac3fe1331d7eb7be217de71d83f02121706a63b
Steven Sanchez <steven12661@gmail.com>
2021-09-11 13:54:39 -0500

    changed button order in Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,48 +6,50 @@ import {
 
 } from "react-router-dom";
 export default class Login extends Component {
-    handleSubmit = e =>{
+    handleSubmit = e => {
         e.preventDefault();
-        const data ={
+        const data = {
             email: this.email,
-            password : this.password
+            password: this.password
         };
         axios.post('https://api-nodejs-todolist.herokuapp.com/user/login', data)
-        .then(
-            res=> {
-                sessionStorage.setItem('token', res.data.token)
-                console.log("token was stored in sessionStorage")
-            }
-        ).catch(
-            err => {
-                console.log(err);
-            }
-        )
+            .then(
+                res => {
+                    sessionStorage.setItem('token', res.data.token)
+                    console.log("token was stored in sessionStorage")
+                }
+            ).catch(
+                err => {
+                    console.log(err);
+                }
+            )
     }
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <h3>Log in</h3>
 
-
                 <div className="form-group">
                     <label>email</label>
                     <input type="email" className="form-control" placeholder="email@example.com"
                         onChange={e => this.email = e.target.value} />
                 </div>
+
                 <div className="form-group">
                     <label>password</label>
                     <input type="password" className="form-control" placeholder="Password"
                         onChange={e => this.password = e.target.value} />
                 </div>
+
                 <br />
+
                 <div className="d-grid gap-2">
+                    <button className="btn btn-primary btn-block" size="lg">Log In</button>
                     <Link to="/signup" className="d-grid gap-2">
                         <button className="btn btn-primary btn-block" size="lg">Sign up</button>
                     </Link>
-                    <button className="btn btn-primary btn-block" size="lg">Log In</button>
-
                 </div>
+                
             </form>
         )
     }
